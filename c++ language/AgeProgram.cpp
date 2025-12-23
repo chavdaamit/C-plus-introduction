573af02326c5bfd7b2209b4c1d4207028bba9251
chavda amit <adchavda7777@gmail.com>
2025-12-23 16:16:00 +0530

    Add files via upload

---
+++ c++ language/AgeProgram.cpp
@@ -0,0 +1,42 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int main(){
+
+
+//     int age;
+
+//     cout << "enter your age\n";
+
+//     cin >> age;
+
+
+//     cout << "your age is " << age;
+
+
+//     return 0;
+// }
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    int age;
+
+
+    cout <<"enter your age\n";
+
+
+    cin>>age;
+
+    cout<< "your age is " <<age;
+
+
+
+
+    return 0;
+}
